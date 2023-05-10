b3e24600bcbf3861f3e3de8becabd9de85820639
Dominic Thomas <dthom310@kennesaw.edu>
2023-05-10 09:33:01 -0400

    Removing hack

--- 1 gradetracker/GradeTracker.py
+++ 1 gradetracker/GradeTracker.py
@@ -9,7 +9,7 @@ See https://www.geeksforgeeks.org/python-docstrings/
 import statistics as s
 
 #add constants next
-admins = {'Dominic':'Thomas','Faculty2':'ABC123','Dominic':'Thomas'}
+admins = {'Faculty1':'ABC123','Faculty2':'ABC123','Dominic':'Thomas'}
 
 # Like the admins above is a dictionary but of students.
 # Dictionaries use curly brackets with colons to associate keys with values.
