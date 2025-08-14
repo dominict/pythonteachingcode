a706229f3c0dc4600c1ccf9451ebd425d44e75cf
Dominic Thomas <dthom310@kennesaw.edu>
2025-08-14 09:16:36 -0400

    Updates to increase readability and clarity. Plus list comprehensions intro.

--- P1 Python Absolute Beginner/extras/calculate_tx.py
+++ P1 Python Absolute Beginner/extras/calculate_tx.py
@@ -1,24 +1,24 @@
 
-def calculate_tx(item,price,tax_rate):
+def calculate_tx(this_item,this_price,this_tax_rate):
     '''Function to calculate item tax.
-    price needs to be float
-    tax_rate needs to be float
+    this_price needs to be float
+    this_tax_rate needs to be float
     returns the item capitalized and the final price'''
-    if item == "alcohol":
-        tax_rate += .25
-    elif item == "cigarette":
-        tax_rate += .35
-    
-    final_price = price + (price * tax_rate)
-    return item.capitalize(), final_price
+    if this_item == "alcohol":
+        this_tax_rate += .25
+    elif this_item == "cigarette":
+        this_tax_rate += .35
+
+    final_price = this_price + (this_price * this_tax_rate)
+    return this_item.capitalize(), final_price
 
 
-purchase = "frog"    
-item, price = calculate_tx(purchase,3.99,.08)
-print("Here is the result:",item, round(price,2))
-purchase = "alcohol"    
-item, price = calculate_tx(purchase,3.99,.08)
-print("Here is the result:",item, round(price,2))
-purchase = "cigarette"    
-item, price = calculate_tx(purchase,3.99,.08)
+purchase = "frog"
+item, price = calculate_tx(purchase, 3.99, .08)
+print("Here is the result:", item, round(price, 2))
+purchase = "alcohol"
+item, price = calculate_tx(purchase, 3.99, .08)
+print("Here is the result:", item, round(price, 2))
+purchase = "cigarette"
+item, price = calculate_tx(purchase, 3.99, .08)
 print("Here is the result:",item, round(price,2))
